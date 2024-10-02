b2f8d7e5870568ccfd863fa00368ddbf6e367254
daniahqaddomi <167869798+daniahqaddomi@users.noreply.github.com>
2024-10-02 15:17:39 +0300

    Add files via upload

---
+++ printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dqaddomi <dqaddomi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 13:32:31 by dqaddomi          #+#    #+#             */
+/*   Updated: 2024/09/22 17:48:23 by dqaddomi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int ft_printf(const char *, ...);
+
+#endif
